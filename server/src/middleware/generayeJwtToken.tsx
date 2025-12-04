2700ae9d8f9ea3f89aaa2d723d178c111bd66c3e
Harivansh <jazzz1815@gmail.com>
2025-12-04 17:33:01 +0530

    check 3

--- server/src/middleware/generayeJwtToken.tsx
+++ server/src/middleware/generayeJwtToken.tsx
@@ -7,6 +7,11 @@ export const generateToken = (userId: string, res: Response) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET as string, {
     expiresIn: "7d",
   });
-  res.cookie("auth", token);
+  res.cookie("auth", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
   return token;
 };
